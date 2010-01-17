684ea7352d5283aa758e06b202f34093dc65d320
sky <sky@localhost.localdomain>
2010-01-17 14:56:57 +0800

    完成git的install脚本。
    下一步是bashrcs的配置

--- admin-scripts/install-git.sh
+++ admin-scripts/install-git.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-. install-common.sh
-
+INSTALLPREFIX=/usr/local
+WGET="wget -q"
 GITSRC=http://kernel.org/pub/software/scm/git/git-1.6.6.tar.bz2
-SOFTSRC=$GITSRC
+SOFTSRC=${GITSRC}
 
 TARFILE=${SOFTSRC##*\/}
 DIRNAME=${TARFILE%.tar.bz2}
-rm -f $TARFILE
-download_or_exit $SOFTSRC
-tar xf $TARFILE && cd ${DIRNAME} && ./configure && make && sudo make install
-rm -rf ${DIRNAME}
+
+if ! type -a git > /dev/null 2>&1 ; then
+	if ! test -e ${TARFILE}; then
+		if ! ${WGET} ${SOFTSRC}; then
+			echo 'download failed'
+			exit 1
+		fi
+	fi
+	tar xf ${TARFILE} && cd ${DIRNAME} && ./configure && make && sudo make install
+	rm -rf ${DIRNAME} ${TARFILE}
+fi
+mkdir -p ${HOME}/codespace && cd ${HOME}/codespace
+if ! test $?; then
+	exit 1
+fi
+git clone http://github.com/thuskyblue/menghanrc.git
