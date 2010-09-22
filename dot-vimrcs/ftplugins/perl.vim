2bd5a22e5482cfa93c932bb4fab96a4beb8986c5
Menghan Zheng <menghan412@gmail.com>
2010-09-22 22:54:03 +0800

    remove vim settings, update emacs setting

--- dot-vimrcs/ftplugins/perl.vim
+++
@@ -1,7 +0,0 @@
-if has('unix')
-	nnoremap <buffer> \rr :!perl -w %<CR>
-	nnoremap <buffer> \rd :!perl -d %<CR>
-endif
-setlocal cindent
-setlocal autoindent
-setlocal nu
--- dot-vimrcs/plugins_src/install.bat
+++
@@ -1,3 +0,0 @@
-copy /y "d:\My Documents\vimrcs\plugins_src\code_complete.vim"  "D:\My Documents\vimfiles\plugin\code_complete.vim" 
-copy /y "d:\My Documents\vimrcs\plugins_src\color\putty.vim"    "D:\My Documents\vimfiles\colors\putty.vim"
-copy /y "d:\My Documents\vimrcs\plugins_src\color\desertEx.vim" "D:\My Documents\vimfiles\colors\desertEx.vim"
