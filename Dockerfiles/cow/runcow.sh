a69de3a68756a5545abd0beb365d8d76579d8fee
Menghan <menghan412@gmail.com>
2014-07-05 19:01:05 +0800

    update cow's Dockerfile

--- Dockerfiles/cow/runcow.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d --privileged --dns=127.0.0.1 -v $HOME/.cow:/.cow -p 7777:7777 cow
--- Dockerfiles/cow/start-all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-service dnsmasq restart
-
-mkdir -p /dev/net
-test -e /dev/net/tun || mknod /dev/net/tun c 10 200
-service openvpn restart
-
-cow -rc /.cow/rc
