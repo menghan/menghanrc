27560835cee44b93ae4a96e015b3e4b7fffcaf0f
Menghan <menghan412@gmail.com>
2014-09-23 00:45:01 +0800

    create all users in 500~4999 range as docker base image

---
+++ Dockerfiles/userbase/addusers.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y adduser
+for ((i=500; i<5000; i++)); do
+	addgroup --system --gid "$i" "$(printf "group%04d" $i)" --quiet
+	adduser --system --no-create-home --disabled-password --disabled-login --uid "$i" --gid "$i" "$(printf "user%04d" $i)" --quiet
+done
+apt-get clean
