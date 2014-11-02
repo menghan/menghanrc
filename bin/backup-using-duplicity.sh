1992b48e7f50729d49f8590ee20e474ced0b34d2
Menghan <menghan412@gmail.com>
2014-11-02 10:41:34 +0800

    add backup script

---
+++ bin/backup-using-duplicity.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -z $3 ]]; then
+	echo "usage: backup src dst container_host_name"
+	exit 1
+fi
+
+docker run --rm \
+	-v "$1":/src:ro \
+	-v "$2":/dst \
+	-v /etc/localtime:/etc/localtime:ro \
+	--hostname "$3" \
+	menghan/duplicity \
+	duplicity --progress --no-encryption --exclude-regexp='/src/.*\.dropbox.*' -v 6 --volsize 100 /src file:///dst
