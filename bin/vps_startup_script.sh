dc6e7dedc76c6a4f60538e4c5318c146fccdb4e8
Menghan <menghan412@gmail.com>
2016-08-18 12:15:57 +0800

    force apt use ipv4

--- bin/vps_startup_script.sh
+++ bin/vps_startup_script.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-apt-get update
-apt-get install -y ansible git
+apt-get update -o Acquire::ForceIPv4=true
+apt-get install -y -o Acquire::ForceIPv4=true ansible git
 
 git clone https://github.com/menghan/menghanrc /var/menghanrc
 ansible-playbook -c local -i localhost, /var/menghanrc/ansible/setup_docker.yml
