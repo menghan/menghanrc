5ec19c14a172bd10615418ff099b42637bb0e090
Menghan <menghan412@gmail.com>
2016-08-18 12:18:35 +0800

    better

--- bin/vps_startup_script.sh
+++ bin/vps_startup_script.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-apt-get update -o Acquire::ForceIPv4=true
-apt-get install -y -o Acquire::ForceIPv4=true ansible git
+echo 'Acquire::ForceIPv4 "true";' | tee /etc/apt/apt.conf.d/99force-ipv4
+apt-get update
+apt-get install -y ansible git
 
 git clone https://github.com/menghan/menghanrc /var/menghanrc
 ansible-playbook -c local -i localhost, /var/menghanrc/ansible/setup_docker.yml
