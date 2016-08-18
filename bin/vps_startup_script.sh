bbe5f8e070bc9863a6df1d0e2f6ea49e07b5ff41
Menghan <menghan412@gmail.com>
2016-08-18 12:21:37 +0800

    allow rerun

--- bin/vps_startup_script.sh
+++ bin/vps_startup_script.sh
@@ -6,5 +6,5 @@ echo 'Acquire::ForceIPv4 "true";' | tee /etc/apt/apt.conf.d/99force-ipv4
 apt-get update
 apt-get install -y ansible git
 
-git clone https://github.com/menghan/menghanrc /var/menghanrc
+git clone https://github.com/menghan/menghanrc /var/menghanrc || (cd /var/menghanrc; git pull)
 ansible-playbook -c local -i localhost, /var/menghanrc/ansible/setup_docker.yml
