c3bc64b9f1fc8cc9ca305adf29a0ee17cdbfcb45
Menghan <menghan412@gmail.com>
2017-04-01 15:34:21 +0800

    update concatyouku.sh

--- bin/concatyouku.sh
+++ bin/concatyouku.sh
@@ -6,13 +6,20 @@
 
 set -e
 
+SED=sed
+MKTEMP=mktemp
+if [[ `uname -a` =~ "Darwin" ]]; then
+	SED=gsed
+	MKTEMP=gmktemp
+fi
+
 count=${1:-1}
 
 for ((i=1; i<=$count; i++))
 do
-	T=$(mktemp -p .)
-	output=$(ls `printf "%02d" $i`*_part*.flv | head -n1 | sed 's/-[a-zA-Z0-9]\{15\}_part[0-9]\{1,\}.flv/.flv/')
-	ls -v1 `printf "%02d" $i`*_part*.flv | sed -e "s/^/file \'/" -e "s/$/\'/" > "$T"
+	T=$($MKTEMP -p .)
+	output=$(ls *`printf "%02d" $i`*_part*.flv | head -n1 | $SED 's/-[a-zA-Z0-9]\{15\}_part[0-9]\{1,\}.flv/.flv/')
+	ls -v1 *`printf "%02d" $i`*_part*.flv | $SED -e "s/^/file \'/" -e "s/$/\'/" > "$T"
 	ffmpeg -f concat -safe -1 -i "$T" -c copy -y "$output"
 	rm -f "$T"
 done
