879baaefaddf7c5a30f31251c1ab65349e8cd099
Menghan <menghan412@gmail.com>
2017-04-03 18:53:13 +0800

    fix

--- bin/concatyouku.sh
+++ bin/concatyouku.sh
@@ -8,9 +8,11 @@ set -e
 
 SED=sed
 MKTEMP=mktemp
+LS=ls
 if [[ `uname -a` =~ "Darwin" ]]; then
 	SED=gsed
 	MKTEMP=gmktemp
+	LS=gls
 fi
 
 count=${1:-1}
@@ -19,7 +21,7 @@ for ((i=1; i<=$count; i++))
 do
 	T=$($MKTEMP -p .)
 	output=$(ls *`printf "%02d" $i`*_part*.flv | head -n1 | $SED 's/-[a-zA-Z0-9]\{15\}_part[0-9]\{1,\}.flv/.flv/')
-	ls -v1 *`printf "%02d" $i`*_part*.flv | $SED -e "s/^/file \'/" -e "s/$/\'/" > "$T"
+	$LS -v1 *`printf "%02d" $i`*_part*.flv | $SED -e "s/^/file \'/" -e "s/$/\'/" > "$T"
 	ffmpeg -f concat -safe -1 -i "$T" -c copy -y "$output"
 	rm -f "$T"
 done
