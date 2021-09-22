7b28eed19c7de4eb8f609a82fa366b87652a75b1
Menghan <menghan412@gmail.com>
2021-09-22 10:43:38 +0800

    fix env for kde

---
+++ kde-env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# link to ~/.config/plasma-workspace/env/
+
+export CLUTTER_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+export QT4_IM_MODULE=ibus
+export QT5_IM_MODULE=ibus
