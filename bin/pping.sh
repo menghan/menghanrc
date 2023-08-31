9958429aba035023569a3603dc4c6847abc04a11
Menghan <menghan412@gmail.com>
2023-08-31 17:33:58 +0800

    fix debug ping count

--- bin/pping.sh
+++ bin/pping.sh
@@ -5,7 +5,7 @@
 while true
 do
 	date
-	ping -c 2 $@ | grep time --line-buffered | tee /tmp/ping$@ | grep time=
+	ping -c 10 $@ | grep time --line-buffered | tee /tmp/ping$@ | grep time=
 	grep -q ', 0% packet loss' /tmp/ping$@ || loss=1
 	if [[ -n $loss ]]; then
 		egrep --color ' [0-9]*% packet loss' /tmp/ping$@
