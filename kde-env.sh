b4511c1983159486dd13383bb80db8f67ecff008
Menghan <menghan412@gmail.com>
2025-04-11 15:48:19 +0800

    update envs for input working on kde6

--- kde-env.sh
+++ kde-env.sh
@@ -4,7 +4,7 @@
 
 export CLUTTER_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
-export GTK_IM_MODULE=ibus
-export QT_IM_MODULE=ibus
+# export GTK_IM_MODULE=ibus
+# export QT_IM_MODULE=ibus
 export QT4_IM_MODULE=ibus
 export QT5_IM_MODULE=ibus
