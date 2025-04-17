2b995efb9096f89e06f9882874d4f3f33271b92c
Menghan <menghan412@gmail.com>
2025-04-17 16:39:00 +0800

    putting shopt into subshell is way more secure and fix a path bug

--- bin/appimage_launcher.sh
+++ bin/appimage_launcher.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
-shopt -s nocaseglob
 
 curr_name="$(basename $0)"
-
-appimage=$(ls -1t $HOME/bin/$curr_name*.AppImage | head -n 1)
+appimage=$(shopt -s nocaseglob; ls -1t $HOME/bin/$curr_name*.AppImage | head -n 1)
 chmod +x "$appimage"
-"$HOME/bin/$appimage" "$@" || "$HOME/bin/$appimage" --no-sandbox "$@"
+"$appimage" "$@" || "$appimage" --no-sandbox "$@"
